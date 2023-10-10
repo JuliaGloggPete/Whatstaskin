218cb0442bfe762e6f54aea9a22993e34b6ff50a
Julia Petersson <113100420+JuliaGloggPete@users.noreply.github.com>
2023-10-10 15:10:42 +0200

    Card

--- src/Pages/ProjectPage.jsx
+++ src/Pages/ProjectPage.jsx
@@ -1,6 +1,7 @@
 import ProjectName from "../components/ProjectName";
 import { useContext } from "react";
 import { DataContext } from "../App";
+import Card from "../components/Card"
 
 function ProjectPage() {
   const [tasks] = useContext(DataContext);
@@ -10,7 +11,8 @@ function ProjectPage() {
   return (
     <>
       <ProjectName title="My awesome project name" />;
-      <main>{tasks && tasks.map((task, i) => <p>{task.taskName}</p>)}</main>
+      
+      <main>{tasks && tasks.map((task, i) => <Card></Card>)}</main>
     </>
   );
 }
