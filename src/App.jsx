f7aeeb1f206f2f02690867819fa5cd94b49e9b16
fabiolapoblete <113706603+fabiolapoblete@users.noreply.github.com>
2023-10-10 14:52:25 +0200

    Loading defualt tasks

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
+import { useEffect, useState } from "react";
 import { createContext } from "react";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import ProjectPage from "./Pages/ProjectPage";
 
 export const DataContext = createContext();
+
 function App() {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    async function fetchTasks() {
+      try {
+        const response = await fetch("tasks.json");
+        const data = await response.json();
+        console.log(data.tasks);
+        setTasks(data.tasks);
+      } catch (error) {
+        console.error(error, "Something went wrong");
+      }
+    }
+    fetchTasks();
+  }, []);
+
   return (
-    <DataContext.Provider>
+    <DataContext.Provider value={[tasks]}>
       <div className="App">
         <Routes>
           <Route path="/" element={<ProjectPage />} />
--- src/Pages/ProjectPage.jsx
+++ src/Pages/ProjectPage.jsx
@@ -1,7 +1,18 @@
 import ProjectName from "../components/ProjectName";
+import { useContext } from "react";
+import { DataContext } from "../App";
 
 function ProjectPage() {
-  return <ProjectName title="My awesome project name" />;
+  const [tasks] = useContext(DataContext);
+
+  console.log(tasks);
+
+  return (
+    <>
+      <ProjectName title="My awesome project name" />;
+      <main>{tasks && tasks.map((task, i) => <p>{task.taskName}</p>)}</main>
+    </>
+  );
 }
 
 export default ProjectPage;
