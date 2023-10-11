f2771a0c688845d8b4eeda89656a16ae00963b51
fabiolapoblete <113706603+fabiolapoblete@users.noreply.github.com>
2023-10-11 10:48:28 +0200

    Removed ;

--- src/Pages/ProjectPage.jsx
+++ src/Pages/ProjectPage.jsx
@@ -4,7 +4,7 @@ import ProjectBoard from "../components/ProjectBoard";
 function ProjectPage() {
   return (
     <>
-      <ProjectName title="My awesome project name" />;
+      <ProjectName title="My awesome project name" />
       <ProjectBoard heading="Development" />
       <ProjectBoard heading="Backlog" />
       <ProjectBoard heading="Done" />
