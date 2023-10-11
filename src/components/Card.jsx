9254996bf3f43ee0fad3a68a05a46cda05d82fee
fabiolapoblete <113706603+fabiolapoblete@users.noreply.github.com>
2023-10-11 12:00:53 +0200

    small change

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,82 +1,70 @@
+import { useEffect, useState } from "react";
 
-import { useEffect, useState } from "react"
-
-import "../assets/Card.css"
-import "../assets/colors.css"
-
+import "../assets/Card.css";
+import "../assets/colors.css";
 
 const Card = (props) => {
+  const [textFields, setTextFields] = useState([]);
+  const [newText, setNewText] = useState("");
+  const [headline, setHeadline] = useState("");
 
+  useEffect(() => {
+    setPrefetchedData();
+  }, []);
 
-    const [textFields, setTextFields] = useState([]);
-    const [newText, setNewText] = useState('');
-    const [headline, setHeadline] = useState('');
-    
-    useEffect (()=> {
-        setPrefetchedData();
-      }, []);
-
-
-    const setPrefetchedData = () => {
+  const setPrefetchedData = () => {
     if (props.task) {
-        console.log(props.task);
-        setHeadline(props.task.taskName);
-        setNewText(props.task.description);
-        newTextField();
+      console.log(props.task);
+      setHeadline(props.task.taskName);
+      setNewText(props.task.description);
+      newTextField();
     }
+  };
+
+  const newTextField = () => {
+    if (newText != "") {
+      setNewText("");
+      console.log("!");
+
+      const newTextField = createTextField(textFields.length);
+
+      setTextFields((textFields) => [...textFields, newTextField]);
+      console.log(textFields);
     }
+  };
 
-    const newTextField = () => {
-        if (newText != '') {
-            setNewText('');
-            console.log('!');
+  const createTextField = () => {
+    const key = Date.now();
+    console.log(key);
+    return (
+      <div className="singleCard" key={key}>
+        <p>{newText}</p>
+        <input type="checkbox"></input>
+      </div>
+    );
+  };
 
-            const newTextField = createTextField(textFields.length);
-        
-            setTextFields(textFields => [...textFields, newTextField]);
-            console.log(textFields);
-        }
-    }
+  const handleInput = (input) => {
+    setNewText(input.target.value);
+  };
 
-
-    const createTextField = () => {
-        const key = Date.now();
-        console.log(key)
-        return (
-            <div className="singleCard" key={key}>
-                <p>{newText}</p>
-                <input type="checkbox"></input>
-                </div>
-
-        )
-    }
-
-    const handleInput = (input) => {
-        setNewText(input.target.value);
-        
-    }
-
-    const handleHeadline = (input) => {
-        setHeadline(input.target.value);
-        console.log(headline)
-    }
-
-
-
+  const handleHeadline = (input) => {
+    setHeadline(input.target.value);
+    console.log(headline);
+  };
 
   return (
     <>
-    <div className="allCards">
-      <input type="text" value={headline} onChange={handleHeadline}></input>
-      <br />
-      {textFields}
-      <input type="text" value={newText} onChange={handleInput}></input>
-      <br />
-      <button onClick={newTextField}>+</button>
+      <div className="allCards">
+        <input type="text" value={headline} onChange={handleHeadline}></input>
+        <br />
+        {textFields}
+        <input type="text" value={newText} onChange={handleInput}></input>
+        <br />
+        <button onClick={newTextField}>+</button>
       </div>
     </>
   );
 };
 
 export default Card;
-
